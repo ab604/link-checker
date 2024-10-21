b64421e4e983d8e80c9cfd2934aa0196f713bfb9
ab604 <ab604@soton.ac.uk>
2024-10-21 12:42:59 +0100

    update az

--- external_link_checker.py
+++ external_link_checker.py
@@ -27,7 +27,7 @@ async def get_links(session, url):
             text = decode_content(content)
         soup = BeautifulSoup(text, 'html.parser')
         
-        main_content = soup.find('div', {'id': 's-lib-public-main'})
+        main_content = soup.find('div', {'id': 's-lg-az-result'})
         
         if main_content:
             for link in main_content.find_all('a', href=True):
